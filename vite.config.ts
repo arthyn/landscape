59a11ce15c33a8dc550cd6347a7d2e34b2943ac1
Liam Fitzgerald <liam@tlon.io>
2021-08-12 16:30:26 +1000

    meta: proxy to fakeship

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/apps/grid/',
+  server: {
+    proxy: {
+      '^((?!\/apps\/grid).)*$': {
+        target: 'http://localhost:8080'
+      }
+    }
+  },
   plugins: [reactRefresh()]
 })
