4dd0ba43fefe08494ba2308d1af55d601492b446
Liam Fitzgerald <liam@tlon.io>
2021-08-12 16:29:48 +1000

    install: actually install

--- src/pages/Grid.tsx
+++ src/pages/Grid.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react"
+import React, { FunctionComponent, useEffect } from "react"
 import { Nav } from "../components/Nav"
 import { Tile } from "../components/Tile"
 import { AppInfoDialog } from "../dialogs/AppInfoDialog"
@@ -7,9 +7,16 @@ import { useQuery } from "react-query"
 import { getApps } from "../logic/api"
 import { RemoveApp } from "../dialogs/RemoveApp"
 import { SuspendApp } from "../dialogs/SuspendApp"
+import useDocketState, {useCharges} from "../state/docket"
+import _ from 'lodash';
+import {InstallApp} from "../dialogs/InstallApp"
 
 export const Grid: FunctionComponent = () => {
   const { data } = useQuery(['apps'], () => getApps())
+  const charges = useCharges();
+  useEffect(() => {
+    useDocketState.getState().fetchCharges();
+  }, []);
   return (
     <div className="flex flex-col">
       <header className={`w-full flex justify-center items-center h-24 sticky top-0 left-0 z-30`} style={{backgroundColor:'rgba(255,255,255,0.95)', backdropFilter: 'blur(50px)'}}>
@@ -18,17 +25,20 @@ export const Grid: FunctionComponent = () => {
 
       <main className='h-full w-full flex justify-center pt-24 pb-32 relative z-0'>
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6 px-4 md:px-8 w-full max-w-6xl">
-          {data && data.map(app => (
-            <Tile key={app.name} app={app} />
+          {charges && _.map(charges, (charge, desk) => (
+            <Tile key={desk} docket={charge} desk={desk} />
           ))}
         </div>
-        <Route exact path="/app/:appId">
+        <Route exact path="/install/:ship/:desk">
+          <InstallApp />
+        </Route>
+        <Route exact path="/app/:desk">
           <AppInfoDialog />
         </Route>
-        <Route exact path="/app/:appId/suspend">
+        <Route exact path="/app/:desk/suspend">
           <SuspendApp />
         </Route>
-        <Route exact path="/app/:appId/remove">
+        <Route exact path="/app/:desk/remove">
           <RemoveApp />
         </Route>
       </main>
